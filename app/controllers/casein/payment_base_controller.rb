30bd5497bccba1874cc7e977df326015ad32e6cb
centrevillage <centrevillage@gmail.com>
2016-05-30 12:54:08 +0900

    issue #28 replace tab to double space

--- app/controllers/casein/payment_base_controller.rb
+++ app/controllers/casein/payment_base_controller.rb
@@ -2,13 +2,13 @@ module Casein
   class PaymentBaseController < Casein::CaseinController
     def index
       @casein_page_title = 'Payment headers'
-			if params[:search]
-  			@payment_headers = payment_header_search.search(params[:search]).order(sort_order(:user_id)).paginate :page => params[:page]
-			elsif params[:account_name]
-  			@payment_headers = payment_header_search.search_account(params[:account_name]).order(sort_order(:user_id)).paginate :page => params[:page]
-			else
-  			@payment_headers = payment_header_search.order(sort_order(:user_id)).paginate :page => params[:page]
-			end
+      if params[:search]
+        @payment_headers = payment_header_search.search(params[:search]).order(sort_order(:user_id)).paginate :page => params[:page]
+      elsif params[:account_name]
+        @payment_headers = payment_header_search.search_account(params[:account_name]).order(sort_order(:user_id)).paginate :page => params[:page]
+      else
+        @payment_headers = payment_header_search.order(sort_order(:user_id)).paginate :page => params[:page]
+      end
       render action: :index
     end
   
@@ -21,43 +21,43 @@ module Casein
     def new_by_last
       @casein_page_title = 'New payment header'
       last_payment_header = payment_header_find params[:id]
-			@payment_header = last_payment_header.dup
-			@payment_header.slip_no = SlipNo.get_num
+      @payment_header = last_payment_header.dup
+      @payment_header.slip_no = SlipNo.get_num
       @payment_header.user_id = current_user.id
-			if @payment_header.save
-				last_payment_header.payment_parts.each do |part|
-					new_part = part.dup
-					@payment_header.payment_parts << new_part
-				end
-			end
+      if @payment_header.save
+        last_payment_header.payment_parts.each do |part|
+          new_part = part.dup
+          @payment_header.payment_parts << new_part
+        end
+      end
       render action: :show
     end
 
 
-		def add_part
+    def add_part
       @payment_header = payment_header_find(params[:id]) 
       @payment_header.payment_parts << PaymentPart.new
       render action: :show
-		end
+    end
   
     def new
       @casein_page_title = 'Add a new payment header'
-			@account = Account.find(params[:account_id]);
-    	@payment_header = build_payment_header(
-				:user_id => current_user.id,
-				:account_id => @account.id,
-				:slip_no => SlipNo.get_num
-				)
+      @account = Account.find(params[:account_id]);
+      @payment_header = build_payment_header(
+        :user_id => current_user.id,
+        :account_id => @account.id,
+        :slip_no => SlipNo.get_num
+        )
       render action: :new
     end
 
     def create
       @payment_header = build_payment_header(payment_header_params)
-			@payment_header.payment_parts.push PaymentPart.new
+      @payment_header.payment_parts.push PaymentPart.new
     
       if @payment_header.save
         flash[:notice] = 'Payment header created'
-      	redirect_to_payment_show(@payment_header)
+        redirect_to_payment_show(@payment_header)
       else
         flash.now[:warning] = 'There were problems when trying to create a new payment header'
         render action: :new
