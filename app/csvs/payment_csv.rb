3b727ecd9b88ce8091e32390b75e4f238868ab05
centrevillage <centrevillage@gmail.com>
2016-05-30 10:24:56 +0900

    issue #24 prevent nil error

--- app/csvs/payment_csv.rb
+++ app/csvs/payment_csv.rb
@@ -16,7 +16,7 @@ class PaymentCSV
     CSV.generate(:headers => HEADER, :write_headers => true) {|csv|
       payments.find_each do |p|
         p.payment_parts.each do |part|
-          csv << [p.payable_on, p.project.try(:name_and_category), part.item.try(:name), part.amount, p.planned, p.user.try(:name), p.org_name, p.account.try(:name), p.budget_code, p.fee_who_paid, p.my_account.try(:bank) || p.project.try(:my_account).try(:bank), p.comment]
+          csv << [p.payable_on, p.try(:project).try(:name_and_category), part.try(:item).try(:name), part.amount, p.planned, p.try(:user).try(:name), p.org_name, p.try(:account).try(:name), p.budget_code, p.fee_who_paid, p.try(:my_account).try(:bank) || p.try(:project).try(:my_account).try(:bank), p.comment]
         end
       end
     }.encode(Encoding::SJIS)
--- app/csvs/receipt_csv.rb
+++ app/csvs/receipt_csv.rb
@@ -15,7 +15,7 @@ class ReceiptCSV
                             .receipt_on_is_not_null.where(receipt_on: (@from..@to))
     CSV.generate(:headers => HEADER, :write_headers => true) {|csv|
       receipts.find_each do |r|
-        csv << [r.receipt_on, r.project.try(:name_and_category), r.item.try(:name), r.amount, r.user.try(:name), r.account.try(:name), r.my_account.try(:bank) || p.project.try(:my_account).try(:bank), r.comment]
+        csv << [r.receipt_on, r.try(:project).try(:name_and_category), r.try(:item).try(:name), r.amount, r.try(:user).try(:name), r.try(:account).try(:name), r.try(:my_account).try(:bank) || p.try(:project).try(:my_account).try(:bank), r.comment]
       end
     }.encode(Encoding::SJIS)
   end
