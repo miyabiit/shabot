50c3322befef11e697dfd68f042c91f7bafe8aa1
centrevillage <centrevillage@gmail.com>
2016-08-01 12:42:52 +0900

    issue #36 出金明細CSVへの項目追加

--- app/csvs/payment_csv.rb
+++ app/csvs/payment_csv.rb
@@ -1,7 +1,24 @@
 require 'csv'
 
 class PaymentCSV
-  HEADER = ['支払日', 'プロジェクト名', '費目', '金額', '計画区分', '作成者', '請求元法人', '請求先', '予算区分', '振込手数料', '引落元口座', '摘要・目的・効果']
+  HEADER = [
+    '支払日',
+    '伝票No.',
+    'プロジェクト名',
+    '費目',
+    '金額',
+    '計画区分',
+    '作成者',
+    '請求元法人',
+    '請求先',
+    '予算区分',
+    '振込手数料',
+    '引落元口座',
+    '引落元銀行支店名',
+    '引落元銀行種別',
+    '引落元銀行口座番号',
+    '摘要・目的・効果'
+  ]
 
   def initialize(from, to)
     @from = from
@@ -16,7 +33,25 @@ class PaymentCSV
     CSV.generate(:headers => HEADER, :write_headers => true) {|csv|
       payments.find_each do |p|
         p.payment_parts.each do |part|
-          csv << [p.payable_on, p.try(:project).try(:name_and_category), part.try(:item).try(:name), part.amount, p.planned, p.try(:user).try(:name), p.org_name, p.try(:account).try(:name), p.budget_code, p.fee_who_paid, p.try(:my_account).try(:bank) || p.try(:project).try(:my_account).try(:bank), p.comment]
+          my_account = p.my_account || p.try(:project).my_account
+          csv << [
+            p.payable_on,
+            p.slip_no,
+            p.try(:project).try(:name_and_category),
+            part.try(:item).try(:name),
+            part.amount,
+            p.planned,
+            p.try(:user).try(:name),
+            p.org_name,
+            p.try(:account).try(:name),
+            p.budget_code,
+            p.fee_who_paid,
+            my_account.try(:bank),
+            my_account.try(:bank_branch),
+            my_account.try(:category),
+            "=\"#{my_account.try(:ac_no)}\"",
+            p.comment
+          ]
         end
       end
     }.to_cp932
