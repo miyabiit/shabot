a4c51bf3e0e17effb1d8c0feaa97a8c7650aa952
centrevillage <centrevillage@gmail.com>
2016-08-01 15:12:53 +0900

    issue #36 update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :group, "ec2-user"
 
 set :rbenv_custom_path, '/usr/local/rbenv'
 set :rbenv_type, :system
-set :rbenv_ruby, '2.1.6'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
