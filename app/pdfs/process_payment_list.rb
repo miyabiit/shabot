02401c55d5d1596bf81f9271f67f1b31881a2b41
centrevillage <centrevillage@gmail.com>
2016-09-05 16:37:00 +0900

    issue #45 ソート順を「支払日->伝票番号」に変更

--- app/pdfs/process_payment_list.rb
+++ app/pdfs/process_payment_list.rb
@@ -10,7 +10,7 @@ class ProcessPaymentList < Prawn::Document
 		)
     font Rails.root.to_s + '/' + "vendor/fonts/ipaexg.ttf"
 
-    payments = payments.payable_on_is_not_null.eager_load(:project, :account).order("payment_headers.payable_on, projects.name, projects.category, accounts.name")
+    payments = payments.payable_on_is_not_null.eager_load(:project, :account).order("payment_headers.payable_on, payment_headers.slip_no")
 
     report = Report::ProcessPaymentReport.new(self, title, payments, from_date, to_date)
     report.show
