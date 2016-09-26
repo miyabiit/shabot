6a77cfd7b5f5e8066ca95375e48e92992db65951
centrevillage <centrevillage@gmail.com>
2016-09-26 14:53:27 +0900

    issue #71 refactoring

--- app/pdfs/each_day_payment_list.rb
+++ app/pdfs/each_day_payment_list.rb
@@ -1,20 +1,13 @@
-class EachDayPaymentList < Prawn::Document
+class EachDayPaymentList < PDFBase
 
   def initialize(payments)
-    super(
-      :page_size => 'A4', 
-      :page_layout => :portrait,
-      :margin => 30,
-      :left_margin => 60,
-      :bottom_margin => 40
-    )
-    font Rails.root.to_s + '/' + "vendor/fonts/ipaexg.ttf"
+    super()
 
     payments = payments.payable_on_is_not_null.eager_load(:project, :account).order("payment_headers.payable_on, projects.name, projects.category, accounts.name")
 
     report = Report::EachDayProjectPaymentReport.new(self, '月別支払日別出金予定・実績一覧', payments, -> (a) { a.payable_on })
     report.show
 
-    number_pages "<page> / <total>", at: [bounds.right - 100, bounds.top - 8], size: 8, width: 100, align: :right
+    render_page_number
   end
 end
--- app/pdfs/payment_receipt.rb
+++ app/pdfs/payment_receipt.rb
@@ -1,13 +1,7 @@
-class PaymentReceipt < Prawn::Document
+class PaymentReceipt < PDFBase
+
   def initialize(from, to)
-    super(
-      :page_size => 'A4',
-      :page_layout => :portrait,
-      :margin => 30,
-      :left_margin => 60,
-      :bottom_margin => 40
-    )
-    font Rails.root.to_s + '/' + "vendor/fonts/ipaexg.ttf"
+    super()
 
     summary = PaymentReceiptSummary.new(from, to)
     all_project_report = Report::PaymentReceipt::AllProjectReport.new(summary, self)
@@ -22,6 +16,6 @@ class PaymentReceipt < Prawn::Document
       project_detail_report.show
     end
 
-    number_pages "<page> / <total>", at: [bounds.right - 100, bounds.top - 8], size: 8, width: 100, align: :right
+    render_page_number
   end
 end
